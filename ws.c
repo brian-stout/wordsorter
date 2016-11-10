5848c5ab42a0c336c4ca3b20db20c996e6a7f623
Brian Stout <brianjonstout@gmail.com>
2016-11-09 20:29:22 -0500

    Doing some skeleton work for switches.

--- ws.c
+++ ws.c
@@ -1,6 +1,33 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+	int print_limiter = -1;
+	
+	char flag_str[8];
+	int flag_str_ind = 0;
 
+	int c;
+	while(-1 < (c = getopt(argc,argv, "c:rnlsauhp"))) {
+		char *err;
+		switch(c) {
+			case 'c':
+				//strtol value to print_limiter
+			case 'r':
+				//Flag for reverse order
+				//Test for repeat cases
+			case 'n':
+				//Flag for sort by numeric value
+			case 'l':
+			case 's':
+			case 'a':
+			case 'u':
+			case 'h':
+			case 'p':
+				//florish
+		}
+	}
+		
 }
