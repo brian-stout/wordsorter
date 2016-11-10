8f53c7386d982e55c64a51c538d165459ef81e43
Brian Stout <brianjonstout@gmail.com>
2016-11-09 20:35:30 -0500

    More skeleton work on switch statements for -options.  Add a bunch of comments on switch flags to remember what they do later.

--- ws.c
+++ ws.c
@@ -6,8 +6,10 @@ int main(int argc, char *argv[])
 {
 	int print_limiter = -1;
 	
-	char flag_str[8];
-	int flag_str_ind = 0;
+	char sort_flag_str[8];
+	int sort_flag_str_ind = 0;
+	bool unique_words_only = false;
+	bool non_alphanum_strip = false;
 
 	int c;
 	while(-1 < (c = getopt(argc,argv, "c:rnlsauhp"))) {
@@ -21,12 +23,21 @@ int main(int argc, char *argv[])
 			case 'n':
 				//Flag for sort by numeric value
 			case 'l':
+				//Flag for sort by length
 			case 's':
+				//Flag for sort by scrabble score
 			case 'a':
+				//Flag for sorting words lexicographically (The default)
+				//Flag is still here despite being default
+				//Because user may want to sort with a -a afterwards
 			case 'u':
+				//Flag for not printing out duplicates
+				//Probably going to be a flag affecting print so needs it's own flag
 			case 'h':
+				//Prints out help options
 			case 'p':
 				//florish
+				//Flag for striping out non-alphanumeric characters from end of words
 		}
 	}
 		
